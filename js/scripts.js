5f58c6e4b9c2fd54b0a6ec809cb19c139b528e1e
shenjan <yanageraxl@gmail.com>
2017-03-06 15:56:13 +0200

    Fixed navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 document.onreadystatechange = function () {
-   if (document.readyState == "complete") {
-    // document is ready. Do your stuff here
+  if (document.readyState == "complete") {
+
     $('.carousel').carousel({interval: 1000000});
+
+    $("#nav").on("click","a", function (event) {
+      //отменяем стандартную обработку нажатия по ссылке
+      event.preventDefault();
+
+      //забираем идентификатор бока с атрибута href
+      var id  = $(this).attr('href');
+
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      var top = $(id).offset().top;
+
+      //анимируем переход на расстояние - top за 1500 мс
+      $('body,html').animate({scrollTop: top}, 1500);
+    });
+
   }
 }
